de6449252fd1e18d62c6f9ff7275e20d73c9127e
kazuha133 <lolnguyen2890@gmail.com>
2025-11-04 10:50:39 +0700

    chinhphuong

--- NguyenQuocTien/Source.cpp
+++ NguyenQuocTien/Source.cpp
@@ -5,6 +5,7 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+bool laSCP(int n);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
@@ -15,6 +16,9 @@ int main()
 	cout<<"Tong =";
 	cout<<"Hieu ="<<hieu(a,b)<<endl;
 	cout<<"Tich ="<<tich(a,b)<<endl;
+	cout<<"Kiem tra so chinh phuong: \n";
+	cout<< a << (laSCP(a) ? " la" : "khong la")<<" so chinh phuong\n";
+	cout<< b << (laSCP(b) ? " la" : "khong la")<<" so chinh phuong\n";
 	system("pause");
 	return 0;
 }
@@ -23,4 +27,10 @@ int hieu(int a, int b){
 }
 int tich(int a, int b){
 	return a*b;
+}
+bool laSCP(int n){
+	if (n<0)
+		return false;
+	int can = sqrt(n);
+	return can*can==n;
 }
